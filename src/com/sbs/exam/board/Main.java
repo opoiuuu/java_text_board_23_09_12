d8d62a3f29985f980dab4bb32d929fed45cf3b50
opoiuuu <opoiuuu@naver.com>
2023-09-21 02:49:21 +0900

    시작 알림 메시지 출력

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -4,7 +4,10 @@ package com.sbs.exam.board;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("== 자바 텍스트 게시판 ==");
+        System.out.println("== 프로그램 시작 ==");
+        System.out.println("명령)");
+        System.out.println("== 프로그램 종료 ==");
 
     }
 }
