2d488f76c173bb561f39c0bd6019ff82f80993f7
opoiuuu <opoiuuu@naver.com>
2023-09-21 03:06:17 +0900

    명령어 입력 받기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,12 +1,21 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
         System.out.println("== 자바 텍스트 게시판 ==");
         System.out.println("== 프로그램 시작 ==");
-        System.out.println("명령)");
+        System.out.print("명령)");
+        String cmd = sc.nextLine();
+        System.out.println("입력된 명령어 : "+ cmd);
+
+        sc.close();
+
         System.out.println("== 프로그램 종료 ==");
 
     }
